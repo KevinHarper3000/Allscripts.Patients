38260c5fb453202da4b43fec44deac0b703ef18d
KevinHarper3000 <kevin.harper3000@protonmail.com>
2022-06-22 09:46:18 -0400

    Update PatientRepository.cs
    Apply Most Severe Diagnosis per category instead of across categories
    
    Set Most Severe Category to 1 for members without categories

--- Patients.DataAccess/PatientRepository.cs
+++ Patients.DataAccess/PatientRepository.cs
@@ -20,104 +20,111 @@ public class PatientRepository : IPatientRepository
     public List<MostSeverePatientDiagnosisByCategory> GetMostSeverePatientDiagnosesPerCategory(int id)
     {
         return connection.Query<MostSeverePatientDiagnosisByCategory>(
-    @"DECLARE @MemberDiagnosisCategory TABLE
-        (
-           MemberID INT NOT NULL,
-           DiagnosisCategoryID INT NULL,
-           MostSevereDiagnosisID INT NULL
-        );
+        @"DECLARE @MemberCategoryDiagnosis TABLE
+            (
+                MemberID INT NOT NULL,
+                DiagnosisCategoryID INT NULL,
+                DiagnosisID INT NULL
+            );
 
-        DECLARE @MinimumCategoryPerMember TABLE
-        (
-          MemberID INT NOT NULL,
-          MinCategory INT NULL
-        );
+            DECLARE @MinimumCategoryPerMember TABLE
+            (
+                MemberID INT NOT NULL,
+                MinCategory INT NULL
+            );
 
+            DECLARE @MostSevereDiagnosisByCategoryPerPatient TABLE
+            (
+                MemberID INT NOT NULL,
+                DiagnosisCategoryID INT NULL,
+                MostSevereDiagnosisID INT NULL
+            );	
 
-        INSERT INTO @MemberDiagnosisCategory
-        SELECT DISTINCT
-          md.MemberID,
-          sub2.DiagnosisCategoryID,
-          sub3.MostSevereDiagnosisID
+            INSERT INTO @MemberCategoryDiagnosis
+            SELECT DISTINCT
+                  
+                  md.MemberID,
+                  sub2.DiagnosisCategoryID,
+                  sub2.DiagnosisID
 
-          FROM Pulse8TestDB.dbo.MemberDiagnosis AS md
-          LEFT JOIN Pulse8TestDB.dbo.DiagnosisCategoryMap AS dcm
-          ON md.DiagnosisID = dcm.DiagnosisID
-        
-        LEFT JOIN
-        (
-          SELECT
-            DiagnosisCategoryID,
-            MIN(dcm.DiagnosisID) AS dID
-            
             FROM Pulse8TestDB.dbo.MemberDiagnosis AS md
             LEFT JOIN Pulse8TestDB.dbo.DiagnosisCategoryMap AS dcm
             ON md.DiagnosisID = dcm.DiagnosisID
-            GROUP BY DiagnosisCategoryID
-        ) AS sub1
-            
-            ON dcm.DiagnosisID = sub1.dID
-        
-        JOIN
-        (
-          SELECT DISTINCT
-            md.MemberID,
-            dcm.DiagnosisCategoryID,
-            d.DiagnosisID,
-            d.DiagnosisDescription
-            
-            FROM Pulse8TestDB.dbo.MemberDiagnosis AS md
-            LEFT JOIN Pulse8TestDB.dbo.DiagnosisCategoryMap AS dcm
-            ON md.DiagnosisID = dcm.DiagnosisID
-            INNER JOIN Pulse8TestDB.dbo.Diagnosis AS d
-            ON dcm.DiagnosisID = d.DiagnosisID
-        ) AS sub2
-        
-        ON dcm.DiagnosisCategoryID = sub2.DiagnosisCategoryID
-        AND md.MemberID = sub2.MemberID
-        
-        JOIN
-        (
-          SELECT DISTINCT
-            md.MemberID,
-            MIN(md.DiagnosisID) AS MostSevereDiagnosisID
-            FROM Pulse8TestDB.dbo.MemberDiagnosis AS md
-            GROUP BY md.MemberID
-        ) AS sub3
-        ON md.MemberID = sub3.MemberID;
-        
-       INSERT INTO @MinimumCategoryPerMember
-       SELECT
-         MemberID, MIN(DiagnosisCategoryID)
-         FROM @MemberDiagnosisCategory
-         GROUP BY MemberID
-        
-       SELECT DISTINCT
-         m.MemberID,
-         m.FirstName,
-         m.LastName,
-         mdc.MostSevereDiagnosisID,
-         d.DiagnosisDescription AS MostSevereDiagnosisDescription,
-         mdc.DiagnosisCategoryID AS CategoryID,
-         dc.CategoryDescription,
-         dc.CategoryScore,
-         IsMostSevereCategory =
-         CASE
-         WHEN mdc.DiagnosisCategoryID IN
-          (SELECT MinCategory WHERE mdc.MemberID = m.MemberID) THEN 1 ELSE 0 END
-         
-         FROM Pulse8TestDB.dbo.Member AS m
-         LEFT JOIN Pulse8TestDB.dbo.MemberDiagnosis AS md
-         ON m.MemberID = md.MemberID
-         LEFT JOIN @MemberDiagnosisCategory AS mdc
-         ON m.MemberID = mdc.MemberID
-         LEFT JOIN Pulse8TestDB.dbo.Diagnosis AS d
-         ON MostSevereDiagnosisID = d.DiagnosisID
-         LEFT JOIN Pulse8TestDB.dbo.DiagnosisCategory AS dc
-         ON mdc.DiagnosisCategoryID = dc.DiagnosisCategoryID
-         LEFT JOIN @MinimumCategoryPerMember AS mcpm
-         ON m.MemberID = mcpm.MemberID
-         WHERE m.MemberID = @id", new {id})
-         .ToList();
+
+            LEFT JOIN
+            (
+                SELECT DISTINCT
+                md.MemberID,
+                dcm.DiagnosisCategoryID,
+                d.DiagnosisID,
+                d.DiagnosisDescription
+
+                FROM Pulse8TestDB.dbo.MemberDiagnosis AS md
+                LEFT JOIN Pulse8TestDB.dbo.DiagnosisCategoryMap AS dcm
+                ON md.DiagnosisID = dcm.DiagnosisID
+                INNER JOIN Pulse8TestDB.dbo.Diagnosis AS d
+                ON dcm.DiagnosisID = d.DiagnosisID
+            ) AS sub2
+
+            ON dcm.DiagnosisCategoryID = sub2.DiagnosisCategoryID
+            AND md.MemberID = sub2.MemberID
+
+            JOIN
+            (
+                SELECT DISTINCT
+                md.MemberID,
+                MIN(md.DiagnosisID) AS MostSevereDiagnosisID
+                FROM Pulse8TestDB.dbo.MemberDiagnosis AS md
+                GROUP BY md.MemberID
+            ) AS sub3
+            ON md.MemberID = sub3.MemberID;
+
+            INSERT INTO @MinimumCategoryPerMember
+            SELECT
+                MemberID, MIN(DiagnosisCategoryID)
+            FROM @MemberCategoryDiagnosis
+            GROUP BY MemberID
+
+
+            INSERT INTO @MostSevereDiagnosisByCategoryPerPatient
+            SELECT m.MemberID, m.DiagnosisCategoryID, MIN(DiagnosisID) AS MostSevereDiagnosisID
+            FROM @MemberCategoryDiagnosis AS m
+            GROUP BY m.MemberID, m.DiagnosisCategoryID
+
+            SELECT DISTINCT
+                m.MemberID,
+                m.FirstName,
+                m.LastName,
+                mdc.DiagnosisCategoryID AS CategoryID,
+                mdc.MostSevereDiagnosisID,
+                d.DiagnosisDescription AS MostSevereDiagnosisDescription,
+                dc.CategoryDescription,
+                dc.CategoryScore,
+                IsMostSevereCategory = 
+                CASE
+                WHEN mdc.DiagnosisCategoryID IN
+                    (SELECT MinCategory WHERE mdc.MemberID = m.MemberID)
+                    OR m.MemberID NOT IN (SELECT MemberID FROM MemberDiagnosis AS md)  
+                    THEN 1 ELSE 0 END
+
+            FROM Pulse8TestDB.dbo.Member AS m
+            LEFT JOIN @MostSevereDiagnosisByCategoryPerPatient AS mdc
+            ON m.MemberID = mdc.MemberID
+            LEFT JOIN Pulse8TestDB.dbo.Diagnosis AS d
+            ON mdc.MostSevereDiagnosisID = d.DiagnosisID
+            LEFT JOIN Pulse8TestDB.dbo.DiagnosisCategory AS dc
+            ON mdc.DiagnosisCategoryID = dc.DiagnosisCategoryID
+            LEFT JOIN @MinimumCategoryPerMember AS mcpm
+            ON m.MemberID = mcpm.MemberID
+            WHERE 
+            m.MemberID = @id
+            AND
+            (
+                mdc.DiagnosisCategoryID IS NOT NULL
+                OR
+                m.MemberID NOT IN
+                (SELECT MemberID FROM MemberDiagnosis AS md)
+            )", new {id})
+            .ToList();
     }
 }
